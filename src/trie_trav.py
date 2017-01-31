52ab4427a8716298c361544028aa7a031da103d1
Colin Lamont <colinlamont@gmail.com>
2017-01-31 14:27:17 -0800

    added travis.

--- src/trie_trav.py
+++ src/trie_trav.py
@@ -29,7 +29,8 @@ class Trie(object):
         insert(self, string): will insert the input string into the trie. If character in the input string is already present, it will be ignored.
         contains(self, string): will return True if the string is in the trie, False if not.
         size(self): will return the total number of words contained within the trie. 0 if empty.
-        remove(self, string): will remove the given string from the trie. If the word doesn’t exist, will raise an appropriate exception.
+        remove(self, string): will remove the given string from the trie. If the word does not exist, will raise an appropriate exception.
+        traversal(self, start=None): a generator yielding values in the tree in depth-first order.
     """
 
     def __init__(self):
