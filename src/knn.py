fcf395438cbd0173f51be8b71d3351381950728d
Colin Lamont <colinlamont@gmail.com>
2017-02-10 13:05:22 -0800

    prep work doc strings and test info.

---
+++ src/knn.py
@@ -0,0 +1,11 @@
+"""Module for k nearest neighbor."""
+
+# K NEAREST NEIGHBOR (KNN)
+#
+# CodeFellows 401d5
+# Submission Date:
+#
+# Authors:  Colin Lamont <https://github.com/chamberi>
+#           Ben Shields <https://github.com/iamrobinhood12345>
+#
+# URL:
